f74c6dcbf749e94975804ce3f51a750af25b0294
Quan Anh Nguyen <93729938+Inzagini@users.noreply.github.com>
2026-01-21 18:20:23 +0100

    [DEMO] - add shader templates and compile sharder vertex and fragment

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,20 @@
 static constexpr int SCR_WIDTH{800};
 static constexpr int SCR_HEIGHT{600};
 
+const char *vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -39,6 +53,49 @@ int main(int arc, char *argv[])
         return 1;
     }
 
+    //createing sharder and match it with the sharder source
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vertexShader);
+    {
+        int success{};
+        char infolog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, nullptr,infolog);
+            std::cerr << "ERROR SHARDER VERTEX COMPILATION ERROR\n" << infolog;
+        }
+    }
+
+    //creating fragment sharder for colors
+    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fragmentShader);
+    {
+        int success{};
+        char infolog[512];
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, nullptr,infolog);
+            std::cerr << "ERROR SHARDER FRAGMENT COMPILATION ERROR\n" << infolog;
+        }
+    }
+
+    //veritces
+    float vertices[] = {
+         -0.5f, -0.5f, 0.0f,
+          0.5f, -0.5f, 0.0f,
+          0.0f,  0.5f, 0.0f
+    };  
+
+    unsigned int VBO{}, VAO{};
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
@@ -46,6 +103,9 @@ int main(int arc, char *argv[])
         glClearColor(0.2f, 0.0f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+       
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
