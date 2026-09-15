46ae8a401abdc2635d3e45bae4461f8781042bf1
Inzagini <toggo1212@gmail.com>
2026-09-15 17:13:25 +0200

    add: rotation to the object

--- inc/class/GameObject.hpp
+++ inc/class/GameObject.hpp
@@ -1,22 +1,22 @@
 #pragma once
 
 #include "Mesh.hpp"
-#include "Transform.hpp"
 #include "Shader.hpp"
+#include "Transform.hpp"
 
-class GameObject
-{
-    public:
-        GameObject(Mesh &mesh) : mesh(&mesh) {}
-        void    draw();
-        Mesh    *getMesh();
-        Transform getTransform();
-        void      inputHandler(GLFWwindow *window, float &dTime);
+class GameObject {
+public:
+  GameObject(Mesh& mesh) : mesh(&mesh) {}
+  void draw();
+  Mesh* getMesh();
+  Transform getTransform();
+  void inputHandler(GLFWwindow* window, float& dTime);
 
-    private:
-        void movementHandler(GLFWwindow *window, float &dTime);
+private:
+  void movementHandler(GLFWwindow* window, float& dTime);
+  void centerObj();
 
-    private:
-        Mesh        *mesh;
-        Transform   transform;
+private:
+  Mesh* mesh;
+  Transform transform;
 };
--- src/GameObject.cpp
+++ src/GameObject.cpp
@@ -1,63 +1,57 @@
 #include "class/GameObject.hpp"
 
-void GameObject::draw()
-{
-    const float solid {1.0f};
+void GameObject::draw() {
+  const float solid{1.0f};
 
-    glEnable(GL_DEPTH_TEST);
+  glEnable(GL_DEPTH_TEST);
 
-    if (mesh->getOpacity() < solid)
-    {
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glDepthMask(GL_FALSE);
-        mesh->draw();
-        glDepthMask(GL_TRUE);
-        glDisable(GL_BLEND);
-    }
-    else
-        mesh->draw();
+  if (mesh->getOpacity() < solid) {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glDepthMask(GL_FALSE);
+    mesh->draw();
+    glDepthMask(GL_TRUE);
+    glDisable(GL_BLEND);
+  } else
+    mesh->draw();
 }
 
-Mesh *GameObject::getMesh()
-{
-    return mesh;
+Mesh* GameObject::getMesh() { return mesh; }
+
+Transform GameObject::getTransform() { return transform; }
+
+void GameObject::inputHandler(GLFWwindow* window, float& dTime) {
+  movementHandler(window, dTime);
 }
 
-Transform GameObject::getTransform()
-{
-    return transform;
+void GameObject::centerObj() {
+  transform.setPositionX(0);
+  transform.setPositionY(0);
+  transform.setPositionZ(0);
 }
 
-void GameObject::inputHandler(GLFWwindow *window, float &dTime)
-{
-    movementHandler(window, dTime);
-}
+void GameObject::movementHandler(GLFWwindow* window, float& dTime) {
+  float speed = 2.0f;
 
-void GameObject::movementHandler(GLFWwindow *window, float &dTime)
-{
-    float speed = 2.0f;
-
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-    {
-        transform.setPositionY(transform.getPositionY() + speed * dTime);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-    {
-        transform.setPositionY(transform.getPositionY() - speed * dTime);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-    {
-        transform.setPositionX(transform.getPositionX() - speed * dTime);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-    {
-        transform.setPositionX(transform.getPositionX() + speed * dTime);
-    }
-    else if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) //put object to center
-    {
-        transform.setPositionX(0);
-        transform.setPositionY(0);
-        transform.setPositionZ(0);
-    }
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    transform.setPositionY(transform.getPositionY() + speed * dTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    transform.setPositionY(transform.getPositionY() - speed * dTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    transform.setPositionX(transform.getPositionX() - speed * dTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    transform.setPositionX(transform.getPositionX() + speed * dTime);
+  }
+  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+    centerObj();
+  }
+  if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) {
+    transform.setRotationY(transform.getRotationY() - speed * dTime * 10);
+  }
+  if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+    transform.setRotationY(transform.getRotationY() + speed * dTime * 10);
+  }
 }
